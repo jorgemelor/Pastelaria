e54f28b596ff2243414ec3d2e5ee2ba45b35c0e7
jorge.jorge <jorgeribeiro467@gmail.com>
2023-09-14 01:37:04 -0300

    first commit

---
+++ src/test/java/com/br/pastelaria/PastelariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pastelaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastelariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
